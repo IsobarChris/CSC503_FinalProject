628be25f86debf93edf3fb665e0758bd6e1813d4
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2012-03-10 19:58:55 -0600

    Initial Commit

---
+++ CSC503_FinalProject/DPFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DPFAppDelegate.h
+//  CSC503_FinalProject
+//
+//  Created by MacBookPro on 3/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DPFAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ CSC503_FinalProject/DPFAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  DPFAppDelegate.m
+//  CSC503_FinalProject
+//
+//  Created by MacBookPro on 3/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DPFAppDelegate.h"
+
+@implementation DPFAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
